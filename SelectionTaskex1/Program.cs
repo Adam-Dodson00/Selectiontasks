32c78dfcd579a6bac2b5bc320162376a64c07ced
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2021-03-11 17:39:25 +1100

    ex 1 task 1 and 2 complete

--- SelectionTaskex1/Program.cs
+++ SelectionTaskex1/Program.cs
@@ -6,7 +6,20 @@ namespace ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("What is your gender? M/F");
+            string gender = Console.ReadLine();
+
+            int agenum = int.Parse(age);
+
+            if (agenum > 17){
+                Console.WriteLine(gender + " Legally adult age");
+            }
+            else{
+                Console.WriteLine(gender + " Legally not adult age");
+            }
         }
     }
 }
