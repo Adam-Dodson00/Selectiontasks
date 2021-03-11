7237a508b6babb88ecd9cb3c89604d4d51499c54
Adam-Dodson00 <62917191+Adam-Dodson00@users.noreply.github.com>
2021-03-11 17:49:35 +1100

    ex 1 task 3 complete

---
+++ SelectionTaskex1/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Number");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Second Number");
+            string num2 = Console.ReadLine();
+
+            int num1int = int.Parse(num1); 
+            int num2int = int.Parse(num2);
+
+            int sum = num1int + num2int;
+
+            if (sum != 10){
+                if (sum < 10){
+                    Console.WriteLine("Sum of numbers is less than 10");
+                }
+                else{
+                    Console.WriteLine("Sum of numbers is more than 10");
+                }
+            }
+            else{
+                Console.WriteLine("Sum of numbers is equal to 10");
+            }
+        }
+    }
+}
